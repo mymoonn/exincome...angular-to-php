903e56fd5f21e7c91f3be20b7e25a0a484d5c28c
mymoonn <79827744+mymoonn@users.noreply.github.com>
2021-03-23 17:54:19 +0530

    Add files via upload

--- dbconn.php
+++ dbconn.php
@@ -1,17 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "exincome";
 
-    try {
-        $dbhost = 'localhost';
-        $dbname = 'exincome';
-        $dbuser = 'root';
-        $dbpass = '';
-        $conn = new \PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-        $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
-    }
-    catch(PDOException $e) {
-        $isConnected = false;
-        throw new Exception($e->getMessage());
-    }
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
 
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
